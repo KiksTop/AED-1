70368d3dd9f3f0d573a62d13c7796b291e3c48c9
Fernando Arikita <f.arikita@unifesp.br>
2025-09-09 21:11:21 -0300

    Código com ponteiro

--- 1180b.c
+++ 1180b.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
-int main (){
-    printf("Testando commit pelo github\n");
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int *X = (int*) malloc(N * sizeof(int)); // alocação dinâmica
+    if (X == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1;
+    }
+
+    // leitura dos elementos
+    for (int i = 0; i < N; i++) {
+        scanf("%d", (X + i));
+    }
+
+    int menor = *X;   // primeiro elemento
+    int posicao = 0;
+
+    for (int i = 1; i < N; i++) {
+        if (*(X + i) < menor) {
+            menor = *(X + i);
+            posicao = i;
+        }
+    }
+
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", posicao);
+
+    free(X); // libera a memória alocada
     return 0;
 }
